1722d8797f20519a9fb323be1a31c4031627ad39
ChizhovMichael <chizhov.michael@gmail.com>
2021-12-08 21:31:42 +0300

    test func

---
+++ tests/test.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgildero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/08 21:30:59 by tgildero          #+#    #+#             */
+/*   Updated: 2021/12/08 21:31:03 by tgildero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TEST_HPP
+# define TEST_HPP
+
+# include <iostream>
+
+void test_vector();
+void test_map();
+void test_set();
+void test_stack();
+
+void test_compare(bool v);
+
+#endif
